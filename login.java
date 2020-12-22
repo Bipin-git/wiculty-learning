8e3c495407251fba834b416e2ff0e9c94e4b3d33
Bipin <yadavbipin0@gmail.com>
2020-12-22 13:47:01 +0530

    sum program written

---
+++ login.java
@@ -0,0 +1,21 @@
+# this is new file in devops learning with .java extension
+# creating normal program
+import java.lang.*;
+
+Class Sum
+{
+	public void print()
+	{
+		System.out.println("In sum");
+	}
+
+
+}
+Class Main
+{
+	Public static void main(String arg[0])
+	{
+		Sum s=new Sum();
+		s.print();
+	}
+}
