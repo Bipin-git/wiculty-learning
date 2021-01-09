89c1afb9e5ec88a36ddc9612d3e38fa1e4acb0ef
Bipin <yadavbipin0@gmail.com>
2021-01-09 15:33:02 +0530

    int=20

--- loginuser.java
+++ loginuser.java
@@ -3,7 +3,8 @@ public class Login
 {
 	Public static void main(String args[])
 	{
-		System.out.println("welcome to login");
+	int i=20;	
+	System.out.println("welcome to login");
 		return 0;
 	}
 }
